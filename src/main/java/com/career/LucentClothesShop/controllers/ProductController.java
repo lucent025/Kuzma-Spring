ff4f152b39d3f069db0c2801ff71e0c7e14394d2
Lucent <nguyenhohoangluc@gmail.com>
2024-04-14 23:44:24 +0700

    Add landing request and singple product request

--- src/main/java/com/career/LucentClothesShop/controllers/ProductController.java
+++ src/main/java/com/career/LucentClothesShop/controllers/ProductController.java
@@ -1,12 +1,17 @@
 package com.career.LucentClothesShop.controllers;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.career.LucentClothesShop.dto.ProductDTO;
@@ -18,8 +23,14 @@ public class ProductController {
 	@Autowired
 	private ProductService service;
 	
-	@GetMapping
-	public ResponseEntity<List<ProductDTO>> getAllProducts() {
-		return new ResponseEntity<List<ProductDTO>>(service.allProducts(), HttpStatus.OK);
+	@GetMapping("/landing")
+	public ResponseEntity<Page<ProductDTO>> getAllProducts(@RequestParam int page, @RequestParam int limit) {
+		Pageable pageable = PageRequest.of(page, limit);
+		return new ResponseEntity<Page<ProductDTO>>(service.getLimitedProducts(pageable), HttpStatus.OK);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Optional<ProductDTO>> getSingleProduct(@PathVariable int id) {
+		return new ResponseEntity<Optional<ProductDTO>>(service.getSingleProduct(id), HttpStatus.OK);
 	}
 }
--- src/main/java/com/career/LucentClothesShop/services/ProductService.java
+++ src/main/java/com/career/LucentClothesShop/services/ProductService.java
@@ -1,9 +1,10 @@
 package com.career.LucentClothesShop.services;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.career.LucentClothesShop.dto.ProductDTO;
@@ -16,10 +17,13 @@ public class ProductService {
 	@Autowired
 	private ProductRepository productRepository;
 	
-	public List<ProductDTO> allProducts() {
-		List<Products> products = productRepository.findAll();
-		return products.stream()
-                .map(ProductMapper::toDTO)
-                .collect(Collectors.toList());
+	public Page<ProductDTO> getLimitedProducts(Pageable pageable) {
+		Page<Products> products = productRepository.findAll(pageable);
+		return products.map(ProductMapper::toDTO);
+	}
+	
+	public Optional<ProductDTO> getSingleProduct(int id) {
+		Optional<Products> product = productRepository.findById(id);
+		return product.map(ProductMapper::toDTO);
 	}
 }
